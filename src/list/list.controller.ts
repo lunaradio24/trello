4e94a6a71a5f691429b27b55be500906ad6e39b5
gus11als <gus11als>
2024-07-12 15:20:34 +0900

    Add:테스트를 위한 리스트관련 코드추가 ,Feat: 테스트를 위한 코드추가 #5

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,34 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ListService } from './list.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
+import { MoveListDto } from './dto/move-list.dto';
+import { List } from './entities/list.entity';
+import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
 
-@Controller('list')
+@Controller('lists')
 export class ListController {
   constructor(private readonly listService: ListService) {}
 
   @Post()
-  create(@Body() createListDto: CreateListDto) {
-    return this.listService.create(createListDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.listService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.listService.findOne(+id);
+  @UseGuards(AccessTokenGuard)
+  async create(@Body() createListDto: CreateListDto): Promise<List> {
+    return await this.listService.create(createListDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateListDto: UpdateListDto) {
+  @UseGuards(AccessTokenGuard)
+  async update(@Param('id') id: string, @Body() updateListDto: UpdateListDto): Promise<List> {
     return this.listService.update(+id, updateListDto);
   }
 
+  @Patch(':id/move')
+  @UseGuards(AccessTokenGuard)
+  async move(@Param('id') id: string, @Body() moveListDto: MoveListDto): Promise<List> {
+    return this.listService.move(+id, moveListDto);
+  }
+
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @UseGuards(AccessTokenGuard)
+  async remove(@Param('id') id: string): Promise<void> {
     return this.listService.remove(+id);
   }
 }
