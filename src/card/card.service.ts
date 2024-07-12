26c674c49e4bc8a97ae7880cae2d47eb7bf33861
JoYoungJinNode5 <jyj950406@naver.com>
2024-07-12 21:40:12 +0900

    Feat: card CRUD #6

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,53 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Card } from './entities/card.entity';
 import { Repository } from 'typeorm';
-import { User } from 'src/user/entities/user.entity';
-import { List } from 'src/list/entities/list.entity';
+import { CardAssignee } from './entities/card_assignee.entity';
 
 @Injectable()
 export class CardService {
   constructor(
     @InjectRepository(Card)
     private cardRepository: Repository<Card>,
+    @InjectRepository(CardAssignee)
+    private cardAssigneeRepository: Repository<CardAssignee>,
   ) {}
 
-  async createCard(createCardDto: CreateCardDto, user: User): Promise<Card> {
+  async createCard(createCardDto: CreateCardDto): Promise<Card> {
     const { title, listId } = createCardDto;
-    const list = await this.cardRepository.findOne({ where: { id: listId } });
+    const list = await this.cardRepository.findOneBy({ listId });
 
     if (!list) {
-      throw new Error('해당 리스트가 없습니다.');
+      throw new NotFoundException('해당 리스트가 없습니다.');
     }
+    const newCard = await this.cardRepository.findOne({
+      where: { listId },
+      order: { position: 'DESC' },
+    });
+    const position = newCard ? newCard.position * 2 : 1024;
 
     const card = this.cardRepository.create({
-      userId: user.id,
       listId: list.id,
       title: title,
+      position,
     });
 
     return this.cardRepository.save(card);
   }
 
-  async findCardById(id: number): Promise<Card> {
+  async getCardById(id: number): Promise<Card> {
     const card = await this.cardRepository.findOne({
       where: { id },
-      relations: [''],
+      relations: ['checklists', 'attachments', 'cardAssignees', 'comments'],
     });
-    return `This action returns a #${id} card`;
+    if (!card) {
+      throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
+    }
+    return card;
   }
 
-  updateCardById(id: number, updateCardDto: UpdateCardDto) {
-    return `This action updates a #${id} card`;
+  async updateCardById(id: number, updateCardDto: UpdateCardDto): Promise<Card> {
+    const { assigneeId, ...cardData } = updateCardDto;
+
+    // 카드 업데이트
+    const card = await this.cardRepository.update(id, cardData);
+
+    // 카드 존재 확인
+    if (!card) {
+      throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
+    }
+
+    // Assignee 업데이트
+    if (assigneeId) {
+      await this.updateAssignees(id, assigneeId);
+    }
+
+    return this.getCardById(id);
+  }
+
+  private async updateAssignees(cardId: number, assigneeId: number[]): Promise<void> {
+    // 기존 Assignee 삭제
+    await this.cardAssigneeRepository.delete({ cardId });
+
+    // 새로운 Assignee 추가
+    const cardAssignees = assigneeId.map((assigneeId) => ({
+      cardId,
+      assigneeId,
+    }));
+    await this.cardAssigneeRepository.save(cardAssignees);
   }
 
   // moveCardById(id: number) {
   //   return `This action returns a #${id} card`;
+  //   1024*2 증가
+  //  롤백 로직 소수점 아래 자리가 7자리 이하로 떨어지면 롤백
   // }
 
-  removeCardById(id: number) {
-    return `This action removes a #${id} card`;
+  async removeCardById(id: number): Promise<void> {
+    const card = await this.cardRepository.findOneBy({ id });
+    if (!card) {
+      throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
+    }
+
+    await this.cardRepository.softDelete({ id });
   }
 }
--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,12 +1,13 @@
-import { isNotEmpty } from "class-validator";
-import { isString } from "class-validator";
+import { PickType } from "@nestjs/mapped-types";
+import {  IsInt,IsNotEmpty,IsString } from "class-validator";
+import { Card } from "../entities/card.entity";
 
-export class CreateCardDto {
-    @isString()
-    @isNotEmpty({ message: '제목을 입력해주세요.'})
-    readonly title: string;
-
-    @isString()
+export class CreateCardDto extends PickType(Card, ['listId','title']){
+    @IsInt()
+    @IsNotEmpty()
     readonly listId: number;
-
+    
+    @IsString()
+    @IsNotEmpty({ message: '제목을 입력해주세요.'})
+    readonly title: string;
 } 
