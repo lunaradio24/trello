5955c10bf6e85e8ad0f517881bd64292168e236d
GinGer-JSK <jskim5701@gmail.com>
2024-07-12 22:11:50 +0900

    Feat: 내 정보 조회 기능구현 #3

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { createClient } from 'redis';
 import { redisStrategy } from './strategies/redis.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -44,6 +45,7 @@ import { redisStrategy } from './strategies/redis.strategy';
     RefreshTokenStrategy,
     LocalStrategy,
     redisStrategy,
+    JwtStrategy,
     {
       provide: 'REDIS',
       useFactory: (configService: ConfigService) => {
