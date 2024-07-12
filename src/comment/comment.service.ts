c82fb9d8399985fc76cb6a68533d4cee0db2e7fe
Changjun Yeo <lunaradio24@gmail.com>
2024-07-12 15:35:12 +0900

    Add: Comment Entity, Feat: 댓글 CRUD #7

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { CommentDto } from './dto/comment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CommentService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>,
+  ) {}
+
+  async create(userId: number, commentDto: CommentDto) {
+    const { cardId } = commentDto;
+
+    // 존재하는 카드인지 확인
+    const card = await this.commentRepository.findOneBy({ cardId });
+    if (!card) {
+      throw new NotFoundException('존재하지 않는 카드입니다.');
+    }
+
+    return await this.commentRepository.save({ commenterId: userId, ...commentDto });
   }
 
-  findAll() {
-    return `This action returns all comment`;
+  async getListByCardId(cardId: number) {
+    // 존재하는 카드인지 확인
+    const card = await this.commentRepository.findOneBy({ cardId });
+    if (!card) {
+      throw new NotFoundException('존재하지 않는 카드입니다.');
+    }
+
+    return await this.commentRepository.findBy({ cardId });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  async getListByCommenterId(commenterId: number) {
+    return await this.commentRepository.findBy({ commenterId });
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(userId: number, commentId: number, commentDto: CommentDto) {
+    // 존재하는 카드인지 확인
+    const { cardId } = commentDto;
+    const card = await this.commentRepository.findOneBy({ cardId });
+    if (!card) {
+      throw new NotFoundException('존재하지 않는 카드입니다.');
+    }
+
+    // 댓글 작성자 본인인지 확인
+    if (card.commenterId !== userId) {
+      throw new ForbiddenException('접근 권한이 없습니다.');
+    }
+
+    return await this.commentRepository.update({ id: commentId }, commentDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(userId: number, commentId: number, cardId: number) {
+    // 존재하는 카드인지 확인
+    const card = await this.commentRepository.findOneBy({ cardId });
+    if (!card) {
+      throw new NotFoundException('존재하지 않는 카드입니다.');
+    }
+
+    // 댓글 작성자 본인인지 확인
+    if (card.commenterId !== userId) {
+      throw new ForbiddenException('접근 권한이 없습니다.');
+    }
+
+    return await this.commentRepository.softDelete({ id: commentId });
   }
 }
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,42 @@
-export class Comment {}
+import { Card } from 'src/card/entities/card.entity';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cardId: number;
+
+  @Column()
+  commenterId: number;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  @ManyToOne(() => Card, (card) => card.comments)
+  @JoinColumn({ name: 'card_id', referencedColumnName: 'id' })
+  card: Card;
+
+  @ManyToOne(() => User, (user) => user.comments)
+  @JoinColumn({ name: 'commenter_id', referencedColumnName: 'id' })
+  commenter: User;
+}
