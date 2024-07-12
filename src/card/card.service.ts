d63ccd6c0d7732131b423b0b43b1f9f5f116e60e
JoYoungJinNode5 <jyj950406@naver.com>
2024-07-12 16:39:43 +0900

    Perf: cardCreate #6

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Card } from './entities/card.entity';
+import { DeepPartial, Repository } from 'typeorm';
 
 @Injectable()
 export class CardService {
-  createCard(createCardDto: CreateCardDto) {
-    return 'This action adds a new card';
+  constructor(
+    @InjectRepository(Card)
+    private cardRepository: Repository<Card>,
+  ) {}
+
+  async createCard(createCardDto: CreateCardDto): Promise<Card> {
+    const { title } = createCardDto;
+
+    const card = this.cardRepository.create({
+      title,
+    } as DeepPartial<Card>);
+
+    const savedCard = await this.cardRepository.save(card);
+
+    return this.cardRepository.create({
+      card_id: card.id,
+      title: card.title,
+    } as DeepPartial<Card>);
   }
 
   findCardById(id: number) {
