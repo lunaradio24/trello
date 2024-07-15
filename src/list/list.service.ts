51a301d52b31d23a90b24824032a2d7b635b1f5b
gus11als <gus11als>
2024-07-15 21:02:08 +0900

    Fix: 마지막? 로직변경 #5

--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -38,7 +38,7 @@ export class ListService {
   }
 
   async findOne(boardId: number, listId: number): Promise<List> {
-    const list = await this.listsRepository.findOne({ where: { id: listId, boardId } });
+    const list = await this.listsRepository.findOne({ where: { id: listId, boardId }, withDeleted: true });
     if (!list) {
       throw new NotFoundException(`해당 보드의 ${listId} 리스트를 찾을 수 없습니다.`);
     }
@@ -64,38 +64,39 @@ export class ListService {
   async move(listId: number, moveListDto: MoveListDto): Promise<List> {
     const { boardId, targetListId, position } = moveListDto;
 
-    const list = await this.findOne(boardId, listId);
+    const list = await this.listsRepository.findOne({ where: { id: listId, boardId } });
     if (!list) {
       throw new NotFoundException('해당 리스트를 찾을 수 없습니다.');
     }
 
-    const targetBoard = await this.listsRepository.findOne({ where: { id: boardId } });
-    if (!targetBoard) {
-      throw new NotFoundException('옮길 리스트를 찾을 수 없습니다.');
-    }
-
-    const targetLists = await this.listsRepository.find({
+    // 오름차순으로 보드의 모든 리스트를 가져옴
+    const boardLists = await this.listsRepository.find({
       where: { boardId },
       order: { position: 'ASC' },
     });
 
-    if (targetLists.length === 0) {
-      throw new NotFoundException('옮길 리스트가 없습니다.');
-    }
-
-    const targetListIndex = targetLists.findIndex((l) => l.id === targetListId);
-    if (targetListIndex < 0) {
-      throw new BadRequestException('리스트가 이동할 곳을 명확히 입력해주세요.');
-    }
-
     let newPosition: number;
-    if (targetLists.length === 1) {
-      // 옮길 리스트에 다른 리스트가 없다면 새 리스트의 포지션 값을 1024로 설정
+
+    if (boardLists.length === 0) {
+      // 보드에 다른 리스트가 없다면 새 리스트의 포지션 값을 1024로 설정
       newPosition = 1024;
     } else {
-      const targetList = targetLists[targetListIndex];
+      // targetListId가 제공되지 않았을 경우 에러 처리
+      if (!targetListId) {
+        throw new BadRequestException('타겟 리스트 ID를 입력해주세요.');
+      }
+
+      const targetListIndex = boardLists.findIndex((l) => l.id === targetListId);
+
+      if (targetListIndex < 0) {
+        throw new BadRequestException('리스트가 이동할 곳을 명확히 입력해주세요.');
+      }
+
+      const targetList = boardLists[targetListIndex];
+
+      // 리스트가 이동할 곳이 타겟 리스트의 위일 때
       if (position === 'before') {
-        const prevList = targetLists[targetListIndex - 1];
+        const prevList = boardLists[targetListIndex - 1];
 
         if (prevList) {
           newPosition = (prevList.position + targetList.position) / 2;
@@ -103,7 +104,8 @@ export class ListService {
           newPosition = targetList.position / 2;
         }
       } else if (position === 'after') {
-        const nextList = targetLists[targetListIndex + 1];
+        // 리스트가 이동할 곳이 타겟 리스트의 아래일 때
+        const nextList = boardLists[targetListIndex + 1];
 
         if (nextList) {
           newPosition = (targetList.position + nextList.position) / 2;
@@ -111,30 +113,28 @@ export class ListService {
           newPosition = targetList.position * 2;
         }
       } else {
-        throw new Error('Invalid position specified');
+        throw new Error('포지션 값이 올바르지 않습니다.');
       }
     }
 
+    /**
+     * 소수점이 0.2미만으로 떨어지면 position 새로고침
+     * 인덱스에 따라 1024의 2의 제곱으로 생성
+     */
+    if (newPosition % 1 !== 0 && newPosition % 1 < 0.2) {
+      // 포지션 값들을 내림차순으로 정렬
+      boardLists.sort((a, b) => b.position - a.position);
+      const factor = 1024;
+      for (let i = 0; i < boardLists.length; i++) {
+        boardLists[i].position = factor * Math.pow(2, i);
+      }
+      await this.listsRepository.save(boardLists);
+      newPosition = factor * Math.pow(2, boardLists.length);
+    }
+
+    // 리스트의 position 값 업데이트
     list.position = newPosition;
-
-    // Check if rebalancing is needed
-    const minPositionDifference = 1;
-    for (let i = 1; i < targetLists.length; i++) {
-      if (targetLists[i].position - targetLists[i - 1].position < minPositionDifference) {
-        const factor = 1024;
-        for (let j = 0; j < targetLists.length; j++) {
-          targetLists[j].position = factor * (j + 1);
-        }
-        await this.listsRepository.save(targetLists);
-        newPosition = factor * Math.pow(2, targetLists.length);
-        list.position = newPosition;
-        await this.listsRepository.save(list);
-        break;
-      }
-    }
-
-    await this.listsRepository.save(list);
-    return list;
+    return this.listsRepository.save(list);
   }
 
   async findAll(boardId: number) {
