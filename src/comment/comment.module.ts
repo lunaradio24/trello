208488a431806e3a983d809aaab400b2875c272d
Changjun Yeo <lunaradio24@gmail.com>
2024-07-15 09:57:09 +0900

    Fix: async/await 누락된 것 수정, Perf: 예외처리 및 deletedAt 확인 추가 #7

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,10 +3,11 @@ import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
-import { User } from "../user/entities/user.entity";
+import { User } from '../user/entities/user.entity';
+import { Card } from 'src/card/entities/card.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, User])],
+  imports: [TypeOrmModule.forFeature([Comment, Card, User])],
   controllers: [CommentController],
   providers: [CommentService],
 })
