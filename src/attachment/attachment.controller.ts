c000352b16f4161c58339dd4c339fb9c4f829bc3
GinGer-JSK <jskim5701@gmail.com>
2024-07-17 12:10:33 +0900

    Refactor: S3 URL 요청기능 개선 #9

--- src/attachment/attachment.controller.ts
+++ src/attachment/attachment.controller.ts
@@ -1,12 +1,10 @@
 import {
-  Body,
   Controller,
   Delete,
   Get,
   HttpStatus,
   Param,
   ParseIntPipe,
-  Patch,
   Post,
   Request,
   Res,
@@ -20,7 +18,6 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { S3Service } from '../s3/s3.service';
 import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-import { Readable } from 'stream';
 
 @ApiTags('Attachment')
 @Controller('cards/:cardId/attachment')
@@ -31,6 +28,7 @@ export class AttachmentController {
     private readonly s3Service: S3Service,
   ) {}
 
+  // 첨부파일 목록 조회
   @Get('')
   async getAttachments(@Param('cardId', ParseIntPipe) cardId: number) {
     const attachments = await this.attachmentService.getAttachmentsByCardId(cardId);
@@ -42,100 +40,70 @@ export class AttachmentController {
   }
 
   // 첨부파일 업로드
-  @Post('upload')
+  @Post('')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
     @Param('cardId', ParseIntPipe) cardId: number,
     @Request() req: any,
     @UploadedFile() file: Express.MulterS3.File,
   ) {
-    const [fileName, fileExt] = file.originalname.split('.');
-    const fileUrl = await this.s3Service.imageUploadToS3(`${Date.now()}_${fileName}`, file, fileExt);
-
-    const createFile = await this.attachmentService.createAttachment(cardId, fileUrl);
-    return {
-      statusCode: HttpStatus.OK,
-      message: '첨부파일 업로드에 성공했습니다.',
-      fileUrl,
-    };
+    try {
+      const fileExt = file.originalname.split('.').pop();
+      const fileName = file.originalname.slice(0, -(fileExt.length + 1));
+      const uniqueFileName = `${Date.now()}_${fileName}.${fileExt}`; // 파일 이름에 확장자 포함
+      const fileUrl = await this.s3Service.imageUploadToS3(uniqueFileName, file, fileExt);
+      const createFile = await this.attachmentService.createAttachment(cardId, fileUrl);
+      return {
+        statusCode: HttpStatus.OK,
+        message: '첨부파일 업로드에 성공했습니다.',
+        fileUrl,
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: '첨부파일 업로드에 실패했습니다.',
+        error: error.message,
+      };
+    }
   }
 
+  // 첨부파일 삭제
   @Delete(':attachmentId')
   async deleteFile(
     @Param('cardId', ParseIntPipe) cardId: number,
     @Param('attachmentId', ParseIntPipe) attachmentId: number,
   ) {
-    await this.attachmentService.deleteAttachment(cardId, attachmentId);
-    return {
-      statusCode: HttpStatus.OK,
-      message: '첨부파일 삭제에 성공했습니다.',
-    };
+    try {
+      await this.attachmentService.deleteAttachment(cardId, attachmentId);
+      return {
+        statusCode: HttpStatus.OK,
+        message: '첨부파일 삭제에 성공했습니다.',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: '첨부파일 삭제에 실패했습니다.',
+        error: error.message,
+      };
+    }
   }
-  // // 첨부파일 다운로드
-  // @Get(':attachmentId/download')
-  // async downloadFile(@Param('attachmentId', ParseIntPipe) attachmentId: number, @Res() res: Response) {
-  //   const attachment = await this.attachmentService.getAttachmentById(attachmentId);
-  //
-  //   if (!attachment) {
-  //     return res.status(HttpStatus.NOT_FOUND).json({
-  //       statusCode: HttpStatus.NOT_FOUND,
-  //       message: '첨부파일을 찾을 수 없습니다.',
-  //     });
-  //   }
-  //
-  //   const fileBuffer = await this.s3Service.downloadFileFromS3(attachment.fileUrl);
-  //   const fileName = attachment.fileUrl.split('/').pop();
-  //   const fileExt = fileName.split('.').pop();
-  //
-  //   // Content-Type과 Content-Disposition 헤더 설정
-  //   res.setHeader('Content-Type', `image/${fileExt}`);
-  //   res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
-  //
-  //   // 파일 버퍼를 응답으로 전송
-  //   res.send(fileBuffer);
-  // }
-  //
-  // // 첨부파일 다운로드
-  // @Get(':attachmentId/download')
-  // async downloadFile(@Param('attachmentId', ParseIntPipe) attachmentId: number, @Res() res: Response) {
-  //   const attachment = await this.attachmentService.getAttachmentById(attachmentId);
-  //
-  //   if (!attachment) {
-  //     return res.status(HttpStatus.NOT_FOUND).json({
-  //       statusCode: HttpStatus.NOT_FOUND,
-  //       message: '첨부파일을 찾을 수 없습니다.',
-  //     });
-  //   }
-  //
-  //   const { Body, ContentType, FileName } = await this.s3Service.downloadFileFromS3(attachment.fileUrl);
-  //
-  //   // 다운로드를 위해 Content-Disposition 헤더 설정
-  //   res.setHeader('Content-Disposition', `attachment; filename=${FileName}`);
-  //   res.setHeader('Content-Type', ContentType);
-  //
-  //   // 파일 스트림을 응답으로 파이프
-  //   Body.pipe(res);
-  // }
 
-  // 첨부파일 다운로드
-  @Get(':attachmentId/download')
-  async downloadFile(@Param('attachmentId', ParseIntPipe) attachmentId: number, @Res() res: Response) {
+  // 첨부파일 URL 요청
+  @Get(':attachmentId')
+  async getFileUrl(@Param('attachmentId', ParseIntPipe) attachmentId: number, @Res() res: Response) {
     const attachment = await this.attachmentService.getAttachmentById(attachmentId);
-
     if (!attachment) {
       return res.status(HttpStatus.NOT_FOUND).json({
         statusCode: HttpStatus.NOT_FOUND,
         message: '첨부파일을 찾을 수 없습니다.',
       });
     }
-
-    const { Body, ContentType, FileName } = await this.s3Service.downloadFileFromS3(attachment.fileUrl);
-
-    // 다운로드를 위해 Content-Disposition 헤더 설정
-    res.setHeader('Content-Disposition', `attachment; filename="${FileName}"`);
-    res.setHeader('Content-Type', ContentType);
-
-    // 파일 스트림을 응답으로 파이프
-    Body.pipe(res);
+    const fileKey = decodeURIComponent(attachment.fileUrl.split('/').pop());
+    const fileExt = fileKey.split('.').pop();
+    const fileUrl = await this.s3Service.getFileUrl(fileKey);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      fileUrl: fileUrl,
+    });
   }
 }
